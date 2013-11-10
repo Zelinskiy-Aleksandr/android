fc651d7fda2fd2cc566d964f5deb7ff5feee59a6
Sanya <zelinskij_sasha@ukr.net>
2013-11-10 18:13:29 +0200

    Files for new activity

---
+++ Fast_Pay/src/com/example/paymentsolution/Receipt.java
@@ -0,0 +1,34 @@
+package com.example.paymentsolution;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Receipt extends Activity implements OnClickListener{
+    
+	Button exit;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_receipt);
+		exit = (Button)findViewById(R.id.exit);
+		exit.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View view) {
+		Intent intent = new Intent("com.example.paymentsolution.SelectOperation");
+		startActivity(intent);
+		this.finish();
+	}
+
+	
+	
+}
